bcb11099b0dccda452f78888e3d2945d07fc766b
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-08 21:07:18 -0600

    Create Coffee class with size and price attributes

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    # size property
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not value in ["Small","Medium","Large"]:
+            print("size must be Small, Medium, or Large")
+
+        self._size = value
+
+    # price property
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, value):
+        self._price = value
+    
