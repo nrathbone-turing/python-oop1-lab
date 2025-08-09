2d035e7b9b1265d682900e4c23e62cd328d7ca8f
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-08 20:29:57 -0600

    Create Book class with an initialize method using property setters to create instance variables upon instantiation and create a method to turn the page; all tests passing

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    # title property
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        self._title = value
+
+    # page_count property
+    @property
+    def page_count(self):
+        return self._page_count
     
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
         
+        self._page_count = value
+
+    def turn_page(value):
+        print("Flipping the page...wow, you read fast!")
